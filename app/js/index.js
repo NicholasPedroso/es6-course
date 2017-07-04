f040113d41da9e0a6e2570abe6cfddd4291d61b5
Nicholas Pedroso <nicholas@tail.digital>
2017-07-04 18:19:41 -0300

    update section

--- app/js/index.js
+++ app/js/index.js
@@ -4,5 +4,33 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('Title');
-/* coding examples */
+const getUser = () => {
+  return {
+    name: 'John',
+    surname: 'Doe',
+    gender: 'male',
+    address: {
+      country: 'United States',
+      city: 'California',
+      postCode: 'CA',
+      fullAddress: {
+        doorNumber: 22,
+        street: 'LA st'
+      }
+    },
+    age: 29
+  }
+};
+
+const user = getUser();
+
+const {name, age, address : { country : theCountry }, address : { fullAddress : { doorNumber : myDoorNumber }} } = user;
+// const name = user.name;
+// const age = user.age;
+// const country = user.address.country;
+// const doorNumber = user.address.fullAddress.doorNumber;
+
+log(name);
+log(age);
+log(theCountry);
+log(myDoorNumber);
